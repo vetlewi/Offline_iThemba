2ce8d1b2f05dbd9639ead608fedfd5ae56bc9d89
hannahberg <hannah.brg@gmail.com>
2019-01-30 07:52:23 +0100

    more dE-E

---
+++ output_files/tegnesiring_dE.C
@@ -0,0 +1,167 @@
+#include "TFile.h"
+#include "TTree.h"
+#include "TColor.h"
+void tegnesisect_dE(){
+    TFile *f = TFile::Open("Ra226_r1_dE_fcal.root");
+    if (f==0){
+        printf("Error: cannot open file");
+        return;
+    }
+    // f->ls();
+    gStyle->SetPalette(1);
+
+    energy_dE_sect_00->GetXaxis()->CenterTitle();
+	energy_dE_sect_00->SetTitle("dE-sect");
+
+    energy_dE_sect_00->SetLineColor(1);
+    energy_dE_sect_00->Draw("");
+    energy_dE_sect_01->SetLineColor(2);
+    energy_dE_sect_02->SetLineColor(3);
+    energy_dE_sect_03->SetLineColor(4);
+    energy_dE_sect_04->SetLineColor(5);
+    energy_dE_sect_05->SetLineColor(6);
+    energy_dE_sect_06->SetLineColor(7);
+    energy_dE_sect_07->SetLineColor(8);
+    energy_dE_sect_08->SetLineColor(9);
+    energy_dE_sect_09->SetLineColor(10);
+    energy_dE_sect_10->SetLineColor(11);
+    energy_dE_sect_11->SetLineColor(12);
+    energy_dE_sect_12->SetLineColor(13);
+    energy_dE_sect_13->SetLineColor(14);
+    energy_dE_sect_14->SetLineColor(15);
+    energy_dE_sect_15->SetLineColor(0);
+
+    energy_dE_sect_02->Draw("same");
+    energy_dE_sect_03->Draw("same");
+    energy_dE_sect_04->Draw("same");
+    energy_dE_sect_05->Draw("same");
+    energy_dE_sect_06->Draw("same");
+    energy_dE_sect_07->Draw("same");
+    energy_dE_sect_08->Draw("same");
+    energy_dE_sect_09->Draw("same");
+    energy_dE_sect_10->Draw("same");
+    energy_dE_sect_11->Draw("same");
+    energy_dE_sect_12->Draw("same");
+    energy_dE_sect_13->Draw("same");
+    energy_dE_sect_14->Draw("same");
+    energy_dE_sect_15->Draw("same");
+
+    // energy_clover_01D->SetLineColor(4);
+    // energy_clover_01B->Draw("same");
+    // energy_clover_01C->Draw("same");
+    // energy_clover_01D->Draw("same");
+
+
+
+    // energy_clover_02A->SetTitle("Clover_02");
+    // energy_clover_02A->SetLineColor(1);
+    // energy_clover_02A->Draw("");
+    // energy_clover_02B->SetLineColor(2);
+    // energy_clover_02C->SetLineColor(3);
+    // energy_clover_02D->SetLineColor(7);
+    // energy_clover_02B->Draw("same");
+    // energy_clover_02C->Draw("same");
+    // energy_clover_02D->Draw("same");
+//
+//
+    // energy_clover_03A->SetTitle("Clover_03");
+    // energy_clover_03A->SetLineColor(1);
+    // energy_clover_03A->Draw("");
+    // energy_clover_03B->SetLineColor(2);
+    // energy_clover_03C->SetLineColor(3);
+    // energy_clover_03D->SetLineColor(4);
+    // energy_clover_03B->Draw("same");
+    // energy_clover_03C->Draw("same");
+    // energy_clover_03D->Draw("same");
+
+
+    // energy_clover_04A->SetTitle("Clover_04");
+    // energy_clover_04A->SetLineColor(1);
+    // energy_clover_04A->Draw("");
+    // energy_clover_04B->SetLineColor(2);
+    // energy_clover_04C->SetLineColor(3);
+    // energy_clover_04D->SetLineColor(4);
+    // energy_clover_04B->Draw("same");
+    // energy_clover_04C->Draw("same");
+    // energy_clover_04D->Draw("same");
+
+    // energy_clover_05A->SetTitle("Clover_05");
+    // energy_clover_05A->SetLineColor(1);
+    // energy_clover_05A->Draw("");
+    // energy_clover_05B->SetLineColor(2);
+    // energy_clover_05C->SetLineColor(3);
+    // energy_clover_05D->SetLineColor(4);
+    // energy_clover_05B->Draw("same");
+    // energy_clover_05C->Draw("same");
+    // energy_clover_05D->Draw("same");
+
+    // energy_clover_06A->SetTitle("Clover_06");
+    // energy_clover_06A->SetLineColor(1);
+    // energy_clover_06A->Draw("");
+    // energy_clover_06B->SetLineColor(2);
+    // energy_clover_06C->SetLineColor(3);
+    // energy_clover_06D->SetLineColor(4);
+    // energy_clover_06B->Draw("same");
+    // energy_clover_06C->Draw("same");
+    // energy_clover_06D->Draw("same");
+
+//
+//     // energy_clover_02A->Draw("same");
+//     // energy_clover_02B->Draw("same");
+//     // energy_clover_02C->Draw("same");
+//     // energy_clover_02D->Draw("same");
+// LaBr fast
+    // energy_labr_2x2_fs_01->SetTitle("LaBr_2");
+    // energy_labr_2x2_fs_01->SetLineColor(1);
+    // energy_labr_2x2_fs_01->Draw("");
+    // energy_labr_2x2_fs_02->SetLineColor(2);
+    // energy_labr_2x2_fs_03->SetLineColor(3);
+    // energy_labr_2x2_fs_04->SetLineColor(4);
+    // energy_labr_2x2_fs_05->SetLineColor(5);
+    // energy_labr_2x2_fs_06->SetLineColor(6);
+    // energy_labr_2x2_fs_02->Draw("same");
+    // energy_labr_2x2_fs_03->Draw("same");
+    // energy_labr_2x2_fs_04->Draw("same");
+    // energy_labr_2x2_fs_05->Draw("same");
+    // energy_labr_2x2_fs_06->Draw("same");
+
+    // energy_labr_3x8_01->SetTitle("LaBr_3x8");
+    // energy_labr_3x8_01->SetLineColor(1);
+    // energy_labr_3x8_01->Draw("");
+    // energy_labr_3x8_02->SetLineColor(2);
+    // energy_labr_3x8_03->SetLineColor(3);
+    // energy_labr_3x8_04->SetLineColor(4);
+    // energy_labr_3x8_05->SetLineColor(5);
+    // energy_labr_3x8_06->SetLineColor(6);
+    // energy_labr_3x8_02->Draw("same");
+    // energy_labr_3x8_03->Draw("same");
+    // energy_labr_3x8_04->Draw("same");
+    // energy_labr_3x8_05->Draw("same");
+    // energy_labr_3x8_06->Draw("same");
+
+}
+
+
+// energy_clover_01A->Draw("");
+// energy_clover_01B->Draw("same");
+// energy_clover_01C->Draw("same");
+// energy_clover_01D->Draw("same");
+// energy_clover_02A->Draw("same");
+// energy_clover_02B->Draw("same");
+// energy_clover_02C->Draw("same");
+// energy_clover_02D->Draw("same");
+
+// energy_labr_2x2_fs_01->Draw("");energy_clover_01A->SetLineColor(4);
+// energy_labr_2x2_fs_02->Draw("same");
+// energy_labr_2x2_fs_03->Draw("same");
+// energy_labr_2x2_fs_04->Draw("same");
+// energy_labr_2x2_fs_05->Draw("same");
+// energy_labr_2x2_fs_06->Draw("same");
+// energy_labr_2x2_fs_07->Draw("same");
+//
+// energy_labr_3x8_01->Draw("");
+// energy_labr_3x8_02->Draw("same");
+// energy_labr_3x8_03->Draw("same");
+// energy_labr_3x8_04->Draw("same");
+// energy_labr_3x8_05->Draw("same");
+// energy_labr_3x8_06->Draw("same");
